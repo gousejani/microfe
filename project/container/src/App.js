de27b87a424e8192474d8ab662a5308bcb1c7562
Gouse <gousejani13@gmail.com>
2021-11-25 10:45:18 +0530

    added header, prefix prod classnames

--- project/container/src/App.js
+++ project/container/src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
+
+import { StylesProvider, createGenerateClassName } from '@material-ui/styles';
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co',
+})
 
 export default () => {
     return <div>
-        <h1>Hi</h1>
-        <hr />
-        <MarketingApp />
+        <BrowserRouter>
+            <StylesProvider generateClassName={generateClassName}>
+                <Header />
+                <MarketingApp />
+            </StylesProvider>
+        </BrowserRouter>
     </div>
 }
